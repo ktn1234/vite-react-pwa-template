e6a9646c0d843fea269c67841edde8436735378a
Kevin Nguyen <ktn1234@yahoo.com>
2024-03-03 01:30:39 -0600

    :wrench: Added VitePWA templated configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,68 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { ConfigEnv, defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const isProduction = mode === "production";
+  return {
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifestFilename: "manifest.json",
+        includeAssets: ["**/*.{js,css,html,ico,png,svg,jpg,jpeg}"], // For precaching files under publidDir only
+        manifest: {
+          id: "",
+          short_name: "",
+          name: "",
+          description: "",
+          icons: [
+            {
+              src: "assets/manifest-icon-192.maskable.png",
+              sizes: "192x192",
+              type: "image/png",
+              purpose: "any",
+            },
+            {
+              src: "assets/manifest-icon-192.maskable.png",
+              sizes: "192x192",
+              type: "image/png",
+              purpose: "maskable",
+            },
+            {
+              src: "assets/manifest-icon-512.maskable.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any",
+            },
+            {
+              src: "assets/manifest-icon-512.maskable.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+          start_url: "/",
+          scope: "/",
+          lang: "en",
+          display: "standalone",
+          theme_color: "",
+          background_color: "",
+        },
+      }),
+    ],
+    publicDir: "public",
+    build: {
+      sourcemap: !isProduction,
+      outDir: "dist",
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@components": path.resolve(__dirname, "./src/components"),
+      },
+    },
+  };
+});
